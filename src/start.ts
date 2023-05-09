f8d4626edc501a825d6be6edd5fc4247450433f4
Sarah Mogin <mogin@work.co>
2023-05-09 14:04:28 -0400

    Download and save JSON response

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,26 @@
-console.log('This is what would run if your app gets started.');
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+const FIGMA_API_KEY = process.env.FIGMA_API_KEY;
+const FIGMA_FILE_ID = process.env.FIGMA_FILE_ID;
+
+async function getFigmaData() {
+  if (!FIGMA_API_KEY || !FIGMA_FILE_ID) {
+    throw new Error('Missing required environment variables');
+  }
+
+  const result = await fetch('https://api.figma.com/v1/files/' + FIGMA_FILE_ID, {
+    headers: {
+      'X-Figma-Token': FIGMA_API_KEY,
+    },
+    method: 'GET',
+  });
+  const figmaTreeStructure = await result.json();
+  console.log(figmaTreeStructure);
+
+  fs.writeFileSync('figma-res.json', JSON.stringify(figmaTreeStructure, undefined, '\t'));
+}
+
+await getFigmaData();
+
+export {};
