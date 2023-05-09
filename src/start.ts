87467d82dfe67951e8be5e7e73b9057e3d6ca044
Sarah Mogin <mogin@work.co>
2023-05-09 14:56:21 -0400

    Add some more example responses

--- src/start.ts
+++ src/start.ts
@@ -4,12 +4,14 @@ import fs from 'fs';
 const FIGMA_API_KEY = process.env.FIGMA_API_KEY;
 const FIGMA_FILE_ID = process.env.FIGMA_FILE_ID;
 
+const PARAMS = '?ids=183-31000';
+
 async function getFigmaData() {
   if (!FIGMA_API_KEY || !FIGMA_FILE_ID) {
     throw new Error('Missing required environment variables');
   }
 
-  const result = await fetch('https://api.figma.com/v1/files/' + FIGMA_FILE_ID, {
+  const result = await fetch('https://api.figma.com/v1/files/' + FIGMA_FILE_ID + PARAMS, {
     headers: {
       'X-Figma-Token': FIGMA_API_KEY,
     },
@@ -18,7 +20,10 @@ async function getFigmaData() {
   const figmaTreeStructure = await result.json();
   console.log(figmaTreeStructure);
 
-  fs.writeFileSync('figma-res.json', JSON.stringify(figmaTreeStructure, undefined, '\t'));
+  fs.writeFileSync(
+    `figma-res${PARAMS.replace(/[^\w]/g, '-')}.json`,
+    JSON.stringify(figmaTreeStructure, undefined, '\t')
+  );
 }
 
 await getFigmaData();
